1b6e093efb7bdb5ab62b690418287991857c4b22
clemjberg <clemens.jae@gmail.com>
2024-12-27 13:51:37 +0100

    first commit

---
+++ SprigBootServer/carcar-backend/src/main/java/at/carcar/carcarbackend/CarcarBackendApplication.java
@@ -0,0 +1,13 @@
+package at.carcar.carcarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcarBackendApplication.class, args);
+	}
+
+}
---
+++ SprigBootServer/carcar-backend/src/test/java/at/carcar/carcarbackend/CarcarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.carcar.carcarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
