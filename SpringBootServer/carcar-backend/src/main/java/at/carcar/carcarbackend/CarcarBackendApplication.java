100f547a08cb60ab3e06f946a1fcb0ae62c0a0d8
Martn <martin.schoenfeld@gmx.at>
2025-01-18 19:57:15 +0100

    Set Timezone
    Big Timezone

--- SpringBootServer/carcar-backend/src/main/java/at/carcar/carcarbackend/CarcarBackendApplication.java
+++ SpringBootServer/carcar-backend/src/main/java/at/carcar/carcarbackend/CarcarBackendApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CarcarBackendApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC+1"));
 		SpringApplication.run(CarcarBackendApplication.class, args);
 	}
 }
